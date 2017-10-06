097165f06d3b581e38e3ef7cf0f28ec26f5f3db7
colbach <colbach@users.noreply.github.com>
2017-10-06 23:16:32 +0200

    Fehlerverbesserung bei Crop

--- Hauptprogramm/src/reflection/nodedefinitions/images/CropNodeDefinition.java
+++ Hauptprogramm/src/reflection/nodedefinitions/images/CropNodeDefinition.java
@@ -4,6 +4,7 @@ import reflection.common.InOut;
 import reflection.common.API;
 import reflection.common.NodeDefinition;
 import java.awt.Graphics;
+import java.awt.Image;
 import java.awt.image.BufferedImage;
 
 public class CropNodeDefinition implements NodeDefinition {
@@ -130,65 +131,19 @@ public class CropNodeDefinition implements NodeDefinition {
     @Override
     public void run(InOut io, API api) {
 
-        BufferedImage image = (BufferedImage) io.in0(0, null);
-
-        int originalWidth = image.getWidth();
-        int originalHeight = image.getHeight();
-        int outWidth = -1;
-        int outHeight = -1;
-
-        Boolean relative = (Boolean) io.in0(1, false);
-        Boolean verhaeltnissBewahren = (Boolean) io.in0(2, false);
-        Number inWidth = (Number) io.in0(3, null);
-        Number inHeight = (Number) io.in0(4, null);
-
-        if (inWidth == null && inHeight == null) {
-            io.out(0, image);
-            io.out(1, originalWidth);
-            io.out(2, originalHeight);
-
-        } else {
-
-            if (relative) {
-                if (inWidth != null) {
-                    outWidth = (int) (originalWidth * inWidth.doubleValue());
-                }
-                if (inHeight != null) {
-                    outHeight = (int) (originalHeight * inHeight.doubleValue());
-                }
-            } else {
-                if (inWidth != null) {
-                    outWidth = inWidth.intValue();
-                }
-                if (inHeight != null) {
-                    outHeight = inHeight.intValue();
-                }
-            }
-            if (inWidth == null) {
-                if (verhaeltnissBewahren) {
-                    outWidth = (int) (originalWidth * (outHeight / (double) originalHeight));
-                } else {
-                    outWidth = originalWidth;
-                }
-            }
-            if (inHeight == null) {
-                if (verhaeltnissBewahren) {
-                    outHeight = (int) (originalHeight * (outWidth / (double) originalWidth));
-                } else {
-                    outHeight = originalHeight;
-                }
-            }
-
-//            System.out.println("outWidth = " + outWidth);
-//            System.out.println("outHeight = " + outHeight);
-            BufferedImage outImage = new BufferedImage(outWidth, outHeight, image.getType());
-            Graphics g = outImage.getGraphics();
-            g.drawImage(image, 0, 0, outWidth, outHeight, null);
-
-            io.out(0, outImage);
-            io.out(1, outWidth);
-            io.out(2, outHeight);
-        }
+        BufferedImage i = (BufferedImage) io.in0(0, null);
+        int l = io.inN(1, 0).intValue();
+        int r = io.inN(2, 0).intValue();
+        int u = io.inN(3, 0).intValue();
+        int o = io.inN(4, 0).intValue();
+        
+        BufferedImage n = new BufferedImage(i.getWidth() + l + r, i.getHeight() + o + u, i.getType());
+        
+        n.getGraphics().drawImage(i, l, o, null);
+        
+        io.out(0, n);
+        io.out(1, n.getWidth());
+        io.out(2, n.getHeight());
     }
 
 }
